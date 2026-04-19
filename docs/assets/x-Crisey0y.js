c0120792b8047725ae24ab1a7aa361d5ec72db20
MeekNutria <139821410+MeekNutria@users.noreply.github.com>
2026-04-19 10:08:38 -0300

    Add assets/x-Crisey0y.js

---
+++ docs/assets/x-Crisey0y.js
@@ -0,0 +1,1 @@
+import{t as e}from"./createLucideIcon-BZpc7Nj0.js";var t=e(`external-link`,[[`path`,{d:`M15 3h6v6`,key:`1q9fwt`}],[`path`,{d:`M10 14 21 3`,key:`gplh6r`}],[`path`,{d:`M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6`,key:`a6xqqp`}]]),n=e(`x`,[[`path`,{d:`M18 6 6 18`,key:`1bl5f8`}],[`path`,{d:`m6 6 12 12`,key:`d8bk6v`}]]);export{t as n,n as t};
